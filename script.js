372f0ec642a5bd1f09be00ba43d95b9686b3b370
Dylan Morrison <dylanjmorrison30@gmail.com>
2022-05-25 11:43:33 +0100

    initial sketch functionality

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
 const canvas = document.querySelector(".canvas");
+let root = document.documentElement;
 
-console.log(canvas);
+let mouseDown = false;
+let penColor = "#bbb";
 
-canvas.addEventListener("keydown", (e) => {
-    console.log(canvas);
-});
+// REPRESENTS GRID SIZE, DEFUALT 16X16
+let dimensions = 16;
+// SETTING GRID DIMENSIONS
+canvas.style.setProperty("grid-template-columns", `repeat(${dimensions}, 1fr)`);
+
+let pixels = [];
+
+const generateGrid = () => {
+    for (let i = 0; i < dimensions ** 2; i++) {
+        let pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.addEventListener("mousedown", (e) => {
+            pixel.style.backgroundColor = penColor;
+        });
+
+        pixel.addEventListener("mouseenter", (e) => {
+            if (mouseDown) pixel.style.backgroundColor = penColor;
+        });
+        pixels.push(pixel);
+    }
+};
+
+const attachGrid = () => {
+    pixels.forEach((pixel) => {
+        canvas.appendChild(pixel);
+    });
+};
+
+const clearGrid = () => {
+    pixels = [];
+    canvas.children.array.forEach((child) => {
+        console.log(child);
+        // canvas.removeChild(child);
+    });
+};
+
+// RUNS WHEN PAGE LOADS TO INITIATE GRID
+generateGrid();
+attachGrid();
+
+document.addEventListener("mousedown", () => (mouseDown = true));
+document.addEventListener("mouseup", () => (mouseDown = false));
